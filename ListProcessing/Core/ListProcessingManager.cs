c1f24dadee36cad8ac478ce5a85a3955c149878c
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-11-18 00:29:46 +0200

    Added command methods in ListProcessingManager

--- ListProcessing/Core/ListProcessingManager.cs
+++ ListProcessing/Core/ListProcessingManager.cs
@@ -1,6 +1,7 @@
 ﻿namespace ListProcessing.Core
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     public class ListProcessingManager
     {
@@ -14,5 +15,77 @@
         }
 
         //TO DO: all commands as instance methods...
+
+        public string Append(string stringToAppend)
+        {
+            list.Add(stringToAppend);
+           return string.Join(" ", list);
+        }
+
+        public string Count(string searchWord)
+        {
+           
+            int counter = 0;
+
+            foreach (var word in list)
+            {
+                if (word == searchWord)
+                {
+                    counter++;
+                }
+            }
+            return counter.ToString();
+        }
+
+        public string Delete(int index)
+        {
+            list.RemoveAt(index);
+            return string.Join(" ",list);
+        }
+
+        public string End()
+        {
+            return "Finished";
+        }
+
+        public string Insert(int index, string stringToInsert)
+        {
+            list.Insert(index, stringToInsert);
+            return string.Join(" ", list);
+        }
+
+        public string Prepend(string stringToPrepend)
+        {
+            list.Insert(0, stringToPrepend);
+           return string.Join(" ", list);
+        }
+
+        public string Reverse()
+        {
+            list.Reverse();
+            return string.Join(" ", list);
+        }
+
+        public string Sort()
+        {
+            list.OrderBy(w => w);
+            return string.Join(" ", list);
+        }
+
+        public string RollLeft()
+        {
+            string first = list[0];
+            list.RemoveAt(0);
+            list.Add(first);
+           return string.Join(" ", list);
+        }
+
+        public string RollRight()
+        {
+            string last = list.Last();
+            list.RemoveAt(list.Count - 1);
+            list.Insert(0, last);
+            return string.Join(" ", list);
+        }
     }
 }
