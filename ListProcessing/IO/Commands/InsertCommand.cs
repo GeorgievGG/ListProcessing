5a81ebddbaef523e3f54f1f09357c4f2f6687c66
MJordanov81 <marian.jordanov@gmail.com>
2017-11-19 22:44:21 +0200

    InsertCommand restored

---
+++ ListProcessing/IO/Commands/InsertCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace ListProcessing.IO.Commands
+{
+    using ListProcessing.Core;
+
+    public class InsertCommand : Command
+    {
+        private readonly string inputString;
+
+        private readonly int inputIndex;
+
+        public InsertCommand(ListProcessingManager manager, string inputString, int inputIndex) : base(manager)
+        {
+            this.inputString = inputString;
+            this.inputIndex = inputIndex;
+        }
+
+        public override string Execute()
+        {
+            return this.Manager.Insert(inputIndex, inputString);
+        }
+    }
+}
