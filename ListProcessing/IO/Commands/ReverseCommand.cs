6e84bfbb4ba42a40763e6469602e31ebadc97e06
MJordanov81 <marian.jordanov@gmail.com>
2017-11-19 22:34:39 +0200

    Update

---
+++ ListProcessing/IO/Commands/ReverseCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace ListProcessing.IO.Commands
+{
+    using ListProcessing.Core;
+
+    //Test - to be updated
+    public class ReverseCommand : Command
+    {
+        private readonly int inputIndex;
+        private readonly string inputString;
+
+        public ReverseCommand(ListProcessingManager manager, int inputIndex, string inputString) : base(manager)
+        {
+            this.inputIndex = inputIndex;
+            this.inputString = inputString;
+        }
+
+        public override string Execute()
+        {
+            return this.Manager.Reverse();
+        }
+    }
+}
---
+++ ListProcessing/IO/Commands/RollCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace ListProcessing.IO.Commands
+{
+    using ListProcessing.Core;
+
+    public class RollCommand : Command
+    {
+        private readonly string direction;
+
+        public RollCommand(ListProcessingManager manager, string direction) : base(manager)
+        {
+            this.direction = direction;
+        }
+
+        public override string Execute()
+        {
+            return this.Manager.Roll(direction);
+        }
+    }
+}
